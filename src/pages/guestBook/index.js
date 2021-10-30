e3ebbf282fe8d234480fb6b1f6bb40116051ae78
SuJin-Jeon <77964989+Truth-Jeon@users.noreply.github.com>
2021-10-30 20:27:43 +0900

    finished

--- src/pages/guestBook/index.js
+++ src/pages/guestBook/index.js
@@ -6,23 +6,31 @@ import { Table, CardHeader } from "reactstrap";
 import { Row, Container } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 import axios from 'axios';
-import Pagination from "../pagination";
-import { paginate } from "../pagination/paginate";
+import Pagination from '@mui/material/Pagination';
 
 const GuestBook = () => {
     const [boardList, setBoardList] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [postsPerPage] = useState(8);
 
     useEffect(() => {
         axios.post('http://localhost:3001/guestbook').then((response) => {
             setBoardList(response.data);
-            console.log("GuestBook => ",response.data);
         })
     }, []);
 
     const readBoard = (idx) => {
         axios.get(`http://localhost:3001/read/${idx}`);
-        console.log(idx, "<== guestbook (아이디 확인)");
     }
+
+    const handleChange=(event, value) => {
+        setCurrentPage(value);
+    }
+
+    const indexOfLastPost = currentPage * postsPerPage; // 마지막 guestbook의 index 번호
+    const indexOfFirstPost = indexOfLastPost - postsPerPage; // 첫번째 guestbook의 index 번호
+    const currentPosts = boardList.slice(indexOfFirstPost, indexOfLastPost); //각 페이지에서 보여질 포스트 배열
+    const pageNumber = Math.ceil(boardList.length/postsPerPage);
     
     return(
         <>
@@ -33,27 +41,27 @@ const GuestBook = () => {
                         <Table className="guestbook_table mt-5">
                             <thead>
                                 <tr>
-                                    <th className="table_head">닉네임</th>
-                                    <th className="table_head">제목</th>
-                                    <th className="table_head">작성일</th>
-                                    <th className="table_head">조회수</th>
-                                    <th className="table_head">수정</th>
-                                    <th className="table_head">삭제</th>
+                                    <th className="table_head_name">닉네임</th>
+                                    <th className="table_head_title">제목</th>
+                                    <th className="table_head_date">작성일</th>
+                                    <th className="table_head_hit">조회수</th>
+                                    <th className="table_head_modify">수정</th>
+                                    <th className="table_head_delete">삭제</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                    {boardList.map((value, key) => {
+                                    {currentPosts.map((value, key) => {
                                         return(
                                             <tr key={key}>
-                                                <th className="table_body">{value.name}</th>
-                                                <Link to={{ pathname: `/read/${value.id}`}} style={{textDecoration:'none'}}><td className="table_body" onClick={()=> {readBoard(value.id)}}>{value.title}</td></Link>
-                                                <td className="table_body">{value.date}</td>
-                                                <td className="table_body">{value.hit}</td>
+                                                <th className="table_body_name">{value.name}</th>
+                                                <Link to={{ pathname: `/read/${value.id}`}} style={{textDecoration:'none'}}><td className="table_body_title" onClick={()=> {readBoard(value.id)}}>{value.title}</td></Link>
+                                                <td className="table_body_date">{value.date}</td>
+                                                <td className="table_body_hit">{value.hit}</td>
                                                 <td>
-                                                    <Link to={`/updatepasswordcheck/${value.id}`}><button className="table_button">수정</button></Link>
+                                                    <Link to={`/updatepasswordcheck/${value.id}`}><button className="table_button_modify">수정</button></Link>
                                                 </td>
                                                 <td>
-                                                    <Link to={`/deletepasswordcheck/${value.id}`}><button className="table_button">삭제</button></Link>
+                                                    <Link to={`/deletepasswordcheck/${value.id}`}><button className="table_button_delete">삭제</button></Link>
                                                 </td>
                                             </tr>
                                         )
@@ -69,7 +77,8 @@ const GuestBook = () => {
                                 </tr>
                             </tbody>
                         </Table>
-                    </Row>
+                        <Pagination className="pagination" count={pageNumber} defaultpage={indexOfFirstPost} page={currentPage} onChange={handleChange} color="primary" />
+                    </Row> 
                 </CardHeader>
             </Container>
         </>
--- src/pages/read/index.js
+++ src/pages/read/index.js
@@ -4,6 +4,7 @@ import { Link, useParams } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../assets/styles/all.css';
 import '../assets/styles/read_page.css';
+import Pagination from '@mui/material/Pagination';
 
 const Read = (props) => {
     const idx = useParams().id;
@@ -13,6 +14,8 @@ const Read = (props) => {
     const [newName, setNewName] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [newContent, setNewContent] = useState('');
+    const [currentReplyPage, setCurrentReplyPage] = useState(1);
+    const [postsPerReplyPage] = useState(5);
 
 
     useEffect(() => {
@@ -36,6 +39,15 @@ const Read = (props) => {
         setWriteReply([...writeReply, {board_id:idx, name:newName, pw:newPassword, content:newContent}]);
         window.location.reload();
     }
+
+    const handleReplyChange=(event, value) => {
+        setCurrentReplyPage(value);
+    }
+
+    const indexOfLastReplyPost = currentReplyPage * postsPerReplyPage; // 마지막 guestbook의 index 번호
+    const indexOfFirstReplyPost = indexOfLastReplyPost - postsPerReplyPage; // 첫번째 guestbook의 index 번호
+    const currentReplyPosts = replyRead.slice(indexOfFirstReplyPost, indexOfLastReplyPost); //각 페이지에서 보여질 포스트 배열
+    const pageReplyNumber = Math.ceil(replyRead.length/postsPerReplyPage);
 
 
     return(
@@ -62,25 +74,26 @@ const Read = (props) => {
                                 )
                             })}
                             <div className="page_comments_all">
-                                {replyRead.map((value) => {
-                                        return(
-                                            <div className="page_comments">
-                                                <div className="page_comments_id">
-                                                    <p>{value.name}</p>
+                                    {currentReplyPosts.map((value) => {
+                                            return(
+                                                <div className="page_comments">
+                                                    <div className="page_comments_id">
+                                                        <p>{value.name}</p>
+                                                    </div>
+                                                    <div className="page_comments_contents">
+                                                        <p>{value.content}</p>
+                                                    </div>
+                                                    <div className="page_comments_date">
+                                                        <p>{value.date}</p>
+                                                    </div>
+                                                    <div className="page_comments_button">
+                                                        <Link to={`/deletereplypassword/${idx}/${value.id}`}><button className="page_comments_realButton">삭제</button></Link>
+                                                    </div>
                                                 </div>
-                                                <div className="page_comments_contents">
-                                                    <p>{value.content}</p>
-                                                </div>
-                                                <div className="page_comments_date">
-                                                    <p>{value.date}</p>
-                                                </div>
-                                                <div className="page_comments_button">
-                                                    <Link to={`/deletereplypassword/${idx}/${value.id}`}><button className="page_comments_realButton">삭제</button></Link>
-                                                </div>
-                                            </div>
-                                        )
-                                    })
-                                }
+                                            )
+                                        })
+                                    }
+                                    <Pagination className="reply_pagination" count={pageReplyNumber} defaultpage={indexOfFirstReplyPost} page={currentReplyPage} onChange={handleReplyChange} variant="outlined" color="primary"/>
                                 <div className="page_comments_text">
                                     <div>
                                         <div className="page_comments_text_id">
